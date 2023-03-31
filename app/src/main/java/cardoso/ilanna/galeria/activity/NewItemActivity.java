d83d67c436a6d394ea71a54ae2beb7af367d8724
20211TIIMI0206 <cardosoilanna96@gmail.com>
2023-03-31 11:12:25 -0300

    Parei no passo 8

--- app/src/main/java/cardoso/ilanna/galeria/activity/NewItemActivity.java
+++ app/src/main/java/cardoso/ilanna/galeria/activity/NewItemActivity.java
@@ -1,14 +1,16 @@
-package cardoso.ilanna.galeria;
+package cardoso.ilanna.galeria.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import cardoso.ilanna.galeria.R;
+
+public class NewItemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_item);
     }
 }
