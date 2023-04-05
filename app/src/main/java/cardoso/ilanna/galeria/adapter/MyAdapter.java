4cda4c746860f676b91b5971913ffd0665cad69b
20211TIIMI0206 <cardosoilanna96@gmail.com>
2023-04-05 12:55:29 -0300

    Parei no passo 12

---
+++ app/src/main/java/cardoso/ilanna/galeria/adapter/MyAdapter.java
@@ -0,0 +1,46 @@
+package cardoso.ilanna.galeria.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import cardoso.ilanna.galeria.R;
+import cardoso.ilanna.galeria.activity.MainActivity;
+import cardoso.ilanna.galeria.model.MyItem;
+
+public class MyAdapter extends RecyclerView.Adapter {
+    MainActivity mainActivity;
+    List<MyItem> itens;
+
+    public MyAdapter(MainActivity mainActivity, List<MyItem> itens) {
+        this.mainActivity = mainActivity;
+        this.itens = itens;
+    }
+
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mainActivity);
+        View v = inflater.inflate(R.layout.item_list, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        MyItem myItem = itens.get(position);
+        View v = holder.itemView;
+
+        ImageView imvfoto = v.findViewById(R.id.imvFoto);
+        imvfoto.setImageURI(myItem.foto);
+
+        TextView tvTitle = v.findViewById(R.id.tvTitle);
+        tvTitle.setText(myItem.title);
+
+        TextView tvdesc = v.findViewById(R.id.tvDesc);
+        tvdesc.setText(myItem.description);
+    }
+}
